e749325a77686b3c4dc710169a58da0f484edd72
Бажен Левковець <bazhen.paseka@gmail.com>
2020-06-20 17:44:27 +0300

    читаем текст из поля СОМ_порт

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QMessageBox::information(this, "Сообщение RED", "Ура! Нажали кнопку 1!");
+    // QMessageBox::information(this, "Сообщение RED", "Ура! Нажали кнопку 1!");
+
+    // У QT свой тип для работы со строками QString
+       // обращением к элементам на форме всегда идет через ui
+       // очевидно что ui->txtPort -- это указатель на поле для ввода порта
+       // ну а ui->txtPort->text() -- это вызов функции которая вернет текстовое содержимое поля
+       QString port = ui->txtPort->text();
+       // QString очень крутой класс, и позволяет нам склеивать строки на лету
+       // т.е.  "Выбранный порт: " + port -- означает подклеить содержимое txtPort к фразе "Выбранный порт"
+       QMessageBox::information(this, "Сообщение RED-2", "Выбранный порт: " + port);
+
 }
 
 void MainWindow::on_pushButton_2_clicked()
